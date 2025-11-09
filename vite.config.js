8fcaab564a653d21d05ade80c2c5e49464516fd5
ExpeditoZ <expedito.farias.melo2003@gmail.com>
2025-11-09 18:36:34 -0300

    quarto

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/cardapio_online/',
-  build: { outDir: 'docs' }
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true
+  }
 })
